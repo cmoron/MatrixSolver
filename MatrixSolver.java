60241345de0a33fd434e93bb01b0905fd2a7c951
Cyril <cyril@mx15-debian>
2015-04-18 17:44:08 +0200

    Adds First class and modelization

---
+++ MatrixSolver.java
@@ -0,0 +1,12 @@
+/* file name  : MatrixGame.java
+ * authors    : Cyril Moron (cyril@moron.at)
+ * created    : Fri 17 Apr 2015 10:09:34 PM CEST
+ */
+
+/** 
+ * The Matrix Game class.
+ * @author Cyril Moron (cyril@moron.at)
+ */
+class MatrixSolver {
+    
+}
